43ac63367aeaa4fc118ea829be2a7bfe884ab209
neizshya <nurul.adzan24@gmail.com>
2025-07-16 14:43:04 +0700

    remove : complicated code structure

--- news/src/routes/index.tsx
+++ news/src/routes/index.tsx
@@ -1,14 +1,14 @@
 import type { FC } from "react";
 import { Route, Routes } from "react-router";
-import Home from "../pages/home";
-import NewsDetail from "../pages/newsDetail";
+import NewsList from "../components/newsList";
+import ArticleDetail from "../components/articleDetail";
 
 const RoutesIndex: FC = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/article/:slug" element={<NewsDetail />} />
+        <Route path="/" element={<NewsList />} />
+        <Route path="/article/:slug" element={<ArticleDetail />} />
       </Routes>
     </>
   );
